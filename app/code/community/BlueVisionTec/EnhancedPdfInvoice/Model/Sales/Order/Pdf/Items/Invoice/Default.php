8e3821df0f6606247f14a107addd91959c2ca8c2
schnere <schnere@bveml.net>
2013-04-17 15:09:59 +0200

    removed IDE project files

--- app/code/community/BlueVisionTec/EnhancedPdfInvoice/Model/Sales/Order/Pdf/Items/Invoice/Default.php
+++ app/code/community/BlueVisionTec/EnhancedPdfInvoice/Model/Sales/Order/Pdf/Items/Invoice/Default.php
@@ -12,24 +12,20 @@
  * obtain it through the world-wide-web, please send an email
  * to license@magentocommerce.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Sales
+ * @category   BlueVisionTec
+ * @package    BlueVisionTec_EnhancedPdfInvoice
  * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @copyright   Copyright (c) 2012 BlueVisionTec e.U. (http://www.bluevisiontec.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 /**
  * Sales Order Invoice Pdf default items renderer
  *
- * @category   Mage
- * @package    Mage_Sales
+ * @category   BlueVisionTec
+ * @package    BlueVisionTec_EnhancedPdfInvoice
  * @author     Magento Core Team <core@magentocommerce.com>
+ * @author     BlueVisionTec e.U. <magedev@bluevisiontec.eu>
  */
 class BlueVisionTec_EnhancedPdfInvoice_Model_Sales_Order_Pdf_Items_Invoice_Default extends Mage_Sales_Model_Order_Pdf_Items_Invoice_Default
 {
